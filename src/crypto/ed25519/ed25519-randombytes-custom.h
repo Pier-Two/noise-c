0a7173172dfca29f51a466438db80288052e9fcc
uink45 <79078981+uink45@users.noreply.github.com>
2025-05-18 19:27:05 +1000

    Add support for CMake;

--- src/crypto/ed25519/ed25519-randombytes-custom.h
+++ src/crypto/ed25519/ed25519-randombytes-custom.h
@@ -1,8 +1,10 @@
-/*
-	a custom randombytes must implement:
-
-	void ED25519_FN(ed25519_randombytes_unsafe) (void *p, size_t len);
-
-	ed25519_randombytes_unsafe is used by the batch verification function
-	to create random scalars
-*/
+/* ed25519-randombytes-custom.h
+ *
+ * This header satisfies ed25519-donna when ED25519_CUSTOMRANDOM is
+ * defined.  The actual implementation lives in Noise-C’s rand_os.c.
+ */
+#ifndef ED25519_RANDOMBYTES_CUSTOM_H
+#define ED25519_RANDOMBYTES_CUSTOM_H
+#include <stddef.h>
+void ed25519_randombytes_unsafe(void *p, size_t len);
+#endif
